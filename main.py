475de927e3dc942073adee6885c473323a807000
Renan Costa Tavares <r.costa.tavares@gmail.com>
2022-01-09 11:49:15 -0300

    Add functions deposit, withdraw and account_balance

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 
-
-def create_account (number, holder, balance, limit):
+def create_account(number, holder, balance, limit):
     account = {"number": number, "holder": holder, "balance": balance, "limit": limit}
     return account
+
+
+def deposit(account, value):
+    account["balance"] += value
+
+
+def withdraw(account, value):
+    account["balance"] -= value
+
+
+def account_balance(account):
+    print("Account Balance: {}".format(account["balance"]))
