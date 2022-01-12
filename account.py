09fc4cfb612fd945ebc1e676aa3f812243fd8a9f
Renan Costa Tavares <r.costa.tavares@gmail.com>
2022-01-12 20:04:42 -0300

    Add class Account

---
+++ account.py
@@ -0,0 +1,10 @@
+
+
+class Account:
+
+    def __init__(self, number, holder, balance, limit):
+        print("Constructing object...")
+        self.number = number
+        self.holder = holder
+        self.balance = balance
+        self.limit = limit
